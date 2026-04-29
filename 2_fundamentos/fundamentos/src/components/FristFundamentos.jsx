ec2f7196b9d84fcce3d1b6c6708db845d1be2e56
Lucas Corrêa <lucascorreadev26@gmail.com>
2026-04-29 16:24:06 -0300

    Inicio dos fundamentos em React

---
+++ 2_fundamentos/fundamentos/src/components/FristFundamentos.jsx
@@ -0,0 +1,20 @@
+const FristFundamentos = () => {
+
+    // sintaxe para comentarios de 1 linha, fora do JSX - padrao JS
+
+    /* 
+        sinxtaxe para multi comentarios, fora do JSX - padrao JS
+    */
+
+    return(
+        <div>
+            {/* sintaxe para comentar dentrodo JSX */}
+
+            <h1> Fundamentos</h1>
+            <p> Meu Primeiro Component em React</p>
+        </div>
+    )
+}
+
+
+export default FristFundamentos;
